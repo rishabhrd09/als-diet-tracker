987e39cf6e565ecf70ed20a8f734aeb2c4a98261
rishabhrd09 <rishabhdongre.rd@gmail.com>
2025-04-29 23:30:24 -0700

    made changes in settings.py

--- diet_tracker_project/settings.py
+++ diet_tracker_project/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iqtjs6l__v+j35_k78b-$y7t031$w_46&meq!mem%+(1%7ey1@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
